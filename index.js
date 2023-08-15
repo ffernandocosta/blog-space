c706ff5d639e264cbcf1f56e0e56148f708fef57
Fernando Costa <ferndcosta353@gmail.com>
2023-08-15 13:29:06 -0300

    feat: add new post to list of posts

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method:"GET"})
         })
         document.getElementById("blog-list").innerHTML = html
 })
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            document.getElementById("blog-list").innerHTML = `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${document.getElementById("blog-list").innerHTML}
+            `
+        })
+})
